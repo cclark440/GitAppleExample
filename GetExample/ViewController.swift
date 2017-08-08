b0cc97da68198cc59927d5d732e9fa57201210e8
user128577 <user128577@74-80-239-252.ic2net.net>
2017-08-08 16:13:21 -0700

    Initial Commit

---
+++ GetExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GetExample
+//
+//  Created by user128577 on 8/8/17.
+//  Copyright © 2017 user128577. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
